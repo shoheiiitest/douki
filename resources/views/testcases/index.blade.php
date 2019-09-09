47e8d761bb5361252702392a8a757867628335b7
shohei <shohei@shinway.net>
2019-09-09 16:57:28 +0700

    テーブルのボーダーを追加

--- resources/views/testcases/index.blade.php
+++ resources/views/testcases/index.blade.php
@@ -1,11 +1,16 @@
 @extends('Layouts.app')
 @section('title','検索')
 @section('content')
-    <table class="table">
-        <tr>
-            @foreach($headers as $header)
-            <th>{{ $header->col_name  }}</th>
-            @endforeach
-        </tr>
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                @foreach($headers as $header)
+                <th>{{ $header->col_name  }}</th>
+                @endforeach
+            </tr>
+        </thead>
+        <tbody>
+
+        </tbody>
     </table>
 @endsection
