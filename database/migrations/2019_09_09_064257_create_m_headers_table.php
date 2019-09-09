f46f62e33453a54c78b8768f411db06afc97732a
shohei <shohei@shinway.net>
2019-09-09 16:51:22 +0700

    add migrations/2019_09_09_064257_create_m_headers_table.php

--- database/migrations/2019_09_09_064257_create_m_headers_table.php
+++ database/migrations/2019_09_09_064257_create_m_headers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateMHeadersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('m_headers', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('project_id');
+            $table->string('col_name','255');
+            $table->integer('disp_flg');
             $table->timestamps();
         });
     }
@@ -31,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('m_headers');
     }
 }
