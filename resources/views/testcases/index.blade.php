66196459da86302595a602ac613ea491231c814e
shohei <shohei@shinway.net>
2019-09-13 11:55:20 +0700

    bootstrap-nicoテスト

--- resources/views/testcases/index.blade.php
+++ resources/views/testcases/index.blade.php
@@ -3,6 +3,13 @@
 @section('content')
     <div id="CtrIndex">
         <rise-loader :loading="loading"></rise-loader>
+        <div class="card text-white bg-primary mb-3">
+            <div class="card-header">Primary card</div>
+            <div class="card-body">
+                <h4 class="card-title">園田海未</h4>
+                <p class="card-text">16歳の高校2年生。一人称は「私」。腰まで伸ばした、青みがかかった黒のロングヘア。好きな食べ物は穂乃果の家のまんじゅう、嫌いな食べ物は炭酸飲料。</p>
+            </div>
+        </div>
         <span v-cloak >@{{ sheet.sheet_name }}</span>
     <table class="table table-bordered">
         <thead>
